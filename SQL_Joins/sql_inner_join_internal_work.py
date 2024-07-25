0a757f1751b7a76ed339c8cec517cc69ab25b78c
amodkumar <amodkumar009@gmail.com>
2024-07-25 19:18:21 +0530

    files updated with scenarios

--- SQL_Joins/sql_inner_join_internal_work.py
+++ SQL_Joins/sql_inner_join_internal_work.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 from constants import input_table_one, input_table_two
 
+scenario_table_one = [1, 1, 1, 1, 1, 'null', 'null']
+scenario_table_two = [1, 1, 1, 2, 'null']
+
 
 def return_inner_join_results(table_one, table_two):
     """This method illustrates how INNER JOIN Works internally in SQL
@@ -27,12 +30,28 @@ def return_inner_join_results(table_one, table_two):
     return items
 
 
+print("-------------------------------- Scenario : 1 ----------------------------------------\nWhen Tables are: \n")
+print(f"Table One: {input_table_one}")
+print(f"Table Two: {input_table_two}\n")
 tables = return_inner_join_results(input_table_one, input_table_two)
 table_data = pd.DataFrame(tables)
 print(table_data)
 
+print("\n-------------------------------- Scenario : 2 ----------------------------------------\nWhen Tables are: \n")
+print(f"Table One: {scenario_table_one}")
+print(f"Table Two: {scenario_table_two}\n")
+scenario_tables = return_inner_join_results(scenario_table_one, scenario_table_two)
+scenario_table_data = pd.DataFrame(scenario_tables)
+print(scenario_table_data)
+
 # Output results : Total number of records is last_id + 1 i.e, 8
 """
+-------------------------------- Scenario : 1 ----------------------------------------
+When Tables are: 
+
+Table One: [1, 1, 1, 2, 3, 3, 3]
+Table Two: [1, 1, 2, 2, 4, 'null']
+
    table_one_updated_result  table_two_updated_result
 0                         1                         1
 1                         1                         1
@@ -42,4 +61,29 @@ print(table_data)
 5                         1                         1
 6                         2                         2
 7                         2                         2
+
+-------------------------------- Scenario : 2 ----------------------------------------
+When Tables are: 
+
+Table One: [1, 1, 1, 1, 1, 'null', 'null']
+Table Two: [1, 1, 1, 2, 'null']
+
+   table_one_updated_result table_two_updated_result
+0                         1                        1
+1                         1                        1
+2                         1                        1
+3                         1                        1
+4                         1                        1
+5                         1                        1
+6                         1                        1
+7                         1                        1
+8                         1                        1
+9                         1                        1
+10                        1                        1
+11                        1                        1
+12                        1                        1
+13                        1                        1
+14                        1                        1
+15                     null                     null
+16                     null                     null
 """
